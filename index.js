2d7680f71908f27255bc0840e284094fb9034cb8
Everlyn <gachuieve32@gmail.com>
2023-09-18 20:17:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(x){
+  if(x < 400){
+    return "This one is on me!"
+
+  }
+  else if(x >2000&& x<=2500){
+    return "I will gladly take your thirty bucks."
+  }
+  else{
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(x){
+ const message =x==='NYC'?'Ok, sounds good.':'No go.';
+ return message;
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(x){
+  switch (x) {
+    case 'generous':
+      x = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      x = 'Thank you.';
+      break;
+    default:
+      x = 'Bye.';
+  }
+  return x;
+  
 }
